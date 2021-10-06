0e8b607345824b756a135af2561c669d1207a60b
Darren Bowers <darren.bowers@g.austincc.edu>
2021-10-06 15:52:51 -0500

    stubbed out functions

--- functions.cpp
+++ functions.cpp
@@ -6,6 +6,30 @@ Purpose: Program to dynamically create two parallel integer arrays and perform o
 
 #include "functions.h"
 
+int sum_array(int*, const int){
+    return 0;
+}
+
+float avg_array(int*, const int){
+    return 0;
+}
+
+void add_arrays(int*, int*, int*, const int){
+    
+}
+
+void display_array(int*, const int){
+
+}
+
+void fill_random(int*, const int){
+
+}
+
+bool is_positive_integer(char*){
+    return 0;
+}
+
     /*
      * write your function definitions here to create the program as
      * specified in the assignment and shown in the examples
