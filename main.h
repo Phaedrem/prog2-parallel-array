5bb82e0189e34d80c8db5c754d764e3f59826991
Darren Bowers <darren.bowers@g.austincc.edu>
2021-10-06 15:10:56 -0500

    updated includes and uses

--- main.h
+++ main.h
@@ -7,10 +7,20 @@ Purpose: Program to dynamically create two parallel integer arrays and perform o
 #ifndef MAIN_H
 #define MAIN_H
 
-/*
- put all your includes and directives for main here
- */
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include "functions.h"
 
+using std::cout;
+using std::endl;
+using std::atoi;
+using std::srand;
+using std::rand;
+using std::fstream;
 
 #endif /* MAIN_H */
 
