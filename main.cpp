40d72356b71330fe077d4c5944e3584e61355bcd
Darren Bowers <darren.bowers@g.austincc.edu>
2021-10-07 17:32:55 -0500

    removed redundant if else statements

--- main.cpp
+++ main.cpp
@@ -11,25 +11,24 @@ int main(int argc, char** argv) {
     fstream myfile;
     int arraySize;
 
-    if(argc == 2){
-        if(is_positive_integer(argv[1])){
-            arraySize = atoi(argv[1]);
-            int *numbers1 = new int[arraySize];
-            int *numbers2 = new int[arraySize];
-            int *answers= new int[arraySize];
-            cout << "the two arrays created are:" << endl;
-            fill_random(numbers1, arraySize);
-            fill_random(numbers2, arraySize);
-            display_array(numbers1, arraySize);
-            display_array(numbers2, arraySize);
-            cout << endl << "adding the arrays together:" << endl;
-            add_arrays(answers,numbers1,numbers2,arraySize);
-            display_array(answers, arraySize);
-            cout << endl << "adding the arrays together:" << endl;
-            cout << "array 1 sum: " << sum_array(numbers1, arraySize) << endl;
-            cout << "array 1 average: " << avg_array(numbers1, arraySize) << endl;
-            cout << endl <<"array 2 sum: " << sum_array(numbers2, arraySize) << endl;
-            cout << "array 2 average: " << avg_array(numbers2, arraySize) << endl;
+    if(argc == 2 && (is_positive_integer(argv[1]))){
+        arraySize = atoi(argv[1]);
+        int *numbers1 = new int[arraySize];
+        int *numbers2 = new int[arraySize];
+        int *answers= new int[arraySize];
+        cout << "the two arrays created are:" << endl;
+        fill_random(numbers1, arraySize);
+        fill_random(numbers2, arraySize);
+        display_array(numbers1, arraySize);
+        display_array(numbers2, arraySize);
+        cout << endl << "adding the arrays together:" << endl;
+        add_arrays(answers,numbers1,numbers2,arraySize);
+        display_array(answers, arraySize);
+        cout << endl << "adding the arrays together:" << endl;
+        cout << "array 1 sum: " << sum_array(numbers1, arraySize) << endl;
+        cout << "array 1 average: " << avg_array(numbers1, arraySize) << endl;
+        cout << endl <<"array 2 sum: " << sum_array(numbers2, arraySize) << endl;
+        cout << "array 2 average: " << avg_array(numbers2, arraySize) << endl;
 
             delete[] numbers1;
             delete[] numbers2;
@@ -38,10 +37,6 @@ int main(int argc, char** argv) {
         } else{
             cout << "usage: " << argv[0] << " n (where n is a number above 0)" << endl;
         }
-    } else {
-        cout << "usage: " << argv[0] << " n (where n is a number above 0)" << endl;
-    }
-
     return 0;
 }
 
